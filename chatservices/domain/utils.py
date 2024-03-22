90b36e01941531dd00a72f53c4aed85c72de3d78
Saul183 <152949586+Saul183@users.noreply.github.com>
2024-03-22 13:31:41 -0600

    pdfs escaneados

--- chatservices/domain/utils.py
+++ chatservices/domain/utils.py
@@ -1,4 +1,5 @@
 import os
+import io
 import streamlit as st
 
 import chromadb
@@ -7,6 +8,9 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings 
+import fitz  # PyMuPDF
+import pytesseract
+from PIL import Image
 
 FILE_LIST = "archivos.txt"
 INDEX_NAME = 'taller'
@@ -37,18 +41,82 @@ def clean_files(path):
     collection = chroma_client.create_collection(name=INDEX_NAME)
     return True
 
+def is_scanned_pdf(pdf_path):
+    """
+    Determina si un archivo PDF contiene hojas escaneadas
+    """
+    doc = fitz.open(pdf_path)
+    for page in doc:
+        if page.get_text() == "":
+            return True  # Si alguna página no tiene texto, consideramos que es escaneada
+    return False
+
+def extract_text_from_image(image):
+    """
+    Extrae texto de una imagen utilizando OCR
+    """
+    return pytesseract.image_to_string(image)
+
 def text_to_chromadb(pdf):
     temp_dir = tempfile.TemporaryDirectory()
     temp_filepath = os.path.join(temp_dir.name, pdf.name)
     with open(temp_filepath, "wb") as f:
         f.write(pdf.getvalue())
 
-    loader = PyPDFLoader(temp_filepath)
-    text = loader.load()
+    if is_scanned_pdf(temp_filepath):
+        st.write("El archivo PDF contiene hojas escaneadas.")
+        extracted_text = ""
+        doc = fitz.open(temp_filepath)
+        for page_number, page in enumerate(doc):
+            image_list = page.get_images()
+            if image_list:
+                st.write(f"Extrayendo texto de las imágenes en la página {page_number + 1}")
+                for image_index, img in enumerate(image_list):
+                    xref = img[0]
+                    base_image = doc.extract_image(xref)
+                    image_bytes = base_image["image"]
+                    image = Image.open(io.BytesIO(image_bytes))
+                    extracted_text += extract_text_from_image(image)
+        st.write("Texto extraído de las imágenes:", extracted_text)
+        create_embeddings_text(pdf.name, extracted_text) 
+    else:
+        st.write("El archivo PDF no contiene hojas escaneadas.")
+        loader = PyPDFLoader(temp_filepath)
+        scanned_document = loader.load()
+        create_embeddings(pdf.name, scanned_document)  # Llamar a create_embeddings con el documento
+        
+        return True
 
-    with st.spinner(f'Creating embedding for file: {pdf.name}'):
-        create_embeddings(pdf.name, text)
+
+def create_embeddings_text(file_name, text):
+    print(f"Creating embeddings for the file: {file_name}")
+
+    # Inicializar RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=100,
+        length_function=len
+    )
+
+    # Dividir el texto en fragmentos
+    chunks = [text]
+
+    # Inicializar HuggingFaceEmbeddings
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+    )
+
+    # Crear embeddings para el texto y almacenarlos en Chroma
+    Chroma.from_documents(
+        chunks,
+        embeddings,
+        client=chroma_client,
+        collection_name=INDEX_NAME
+    )
+
     return True
+
+
 
 def create_embeddings(file_name, text):
     print(f"Creating embeddings for the file: {file_name}")
